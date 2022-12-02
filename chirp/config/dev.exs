96eafb5599c89fe83ace91b34e60d39860426edf
Annoz <annoz@tutanota.com>
2022-12-02 23:28:22 +0100

    addind tailwind config

--- chirp/config/dev.exs
+++ chirp/config/dev.exs
@@ -26,7 +26,8 @@ config :chirp, ChirpWeb.Endpoint,
   secret_key_base: "Hvk5hi3MEmRbAMDDYNCbd3OpSwtnESAEaVwQgYtt/OacifGf7V0tW3/Bgn5fdmik",
   watchers: [
     # Start the esbuild watcher by calling Esbuild.install_and_run(:default, args)
-    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]}
+    esbuild: {Esbuild, :install_and_run, [:default, ~w(--sourcemap=inline --watch)]},
+    tailwind: {Tailwind, :install_and_run, [:default, ~w(--watch)]}
   ]
 
 # ## SSL Support
